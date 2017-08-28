e34c4de2cd8ff8d66ffe6052e5faa64f5da3ef75
erkajp <erka.j.p@gmail.com>
2017-08-28 14:26:06 +0700

    add selectionReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer.js';
 
 export default combineReducers({
   libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer,
 });
